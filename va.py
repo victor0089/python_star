9c708d0b1f182d3d65f33f5dca3fa9c5b3726835
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-10-12 12:51:27 +0300

    Create va.py

---
+++ va.py
@@ -0,0 +1,50 @@
+
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import webbrowser
+
+def listen():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        query = recognizer.recognize_google(audio)
+        print("You said:", query)
+        return query.lower()
+    except sr.UnknownValueError:
+        print("Sorry, I didn't catch that.")
+        return ""
+    except sr.RequestError:
+        print("Sorry, I'm having trouble accessing the speech recognition service.")
+        return ""
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def process_command(command):
+    if "hello" in command:
+        speak("Hello! How can I assist you today?")
+    elif "time" in command:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"The current time is {current_time}")
+    elif "search" in command:
+        search_query = command.replace("search", "").strip()
+        search_url = f"https://www.google.com/search?q={search_query}"
+        webbrowser.open(search_url)
+    elif "exit" in command:
+        speak("Goodbye!")
+        exit()
+    else:
+        speak("I'm sorry, I can't help with that.")
+
+# Main program loop
+while True:
+    command = listen()
+    process_command(command)
